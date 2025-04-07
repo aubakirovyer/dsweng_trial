f3ae0cd07a9851ec1b3e95ef5654a6a4e852f4ac
aubakirovyer <142869234+aubakirovyer@users.noreply.github.com>
2025-04-07 12:18:06 +0500

    Update main.py - add formatter today's date

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     #today_formatted = today.strftime("%Y-%m-%d %H:%M")
     print(f"Hello, World! Today is {today}")
 
+    # change made by other person
+    today_formatted = today.strftime("%Y-%m-%d %H:%M")
+    print(f"Hello, World2! Today's formatter date is {today_formatted}")
+
 
 if __name__ == "__main__":
     main()
