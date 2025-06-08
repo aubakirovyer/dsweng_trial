2b7fe901ad22bb2c71c36ff67aed03fb420c5f8a
FinnArth <=>
2025-06-08 13:43:08 +0500

    trialcodes created

---
+++ trialcodes/string.py
@@ -0,0 +1,3 @@
+text = "Full string"
+sliced_text = slice(3, 6, 1)
+print(text[sliced_text])
---
+++ trialcodes/tuples.py
@@ -0,0 +1,8 @@
+integer_tuple = (1)
+print(type(integer_tuple))
+
+empty_tuple = ()
+print(type(empty_tuple))
+
+string_tuple = ("here")
+print(type(string_tuple))
