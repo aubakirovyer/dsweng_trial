0470f3d9f603fe3b274e48736f85f7383d463dc8
FinnArth <=>
2025-06-08 15:17:50 +0500

    comparison of tuples and immutable objects with the same value and different ids

--- trialcodes/tuples.py
+++ trialcodes/tuples.py
@@ -14,3 +14,26 @@ print(*some_tuple)
 
 (a,b,c) = some_tuple
 print(a,b,c)
+
+# Comparison of tuples
+tuple1 = (1, 2, 3)
+tuple2 = (1, 2, 3)
+print(tuple1 == tuple2)  # True, because they are equal
+print(id(tuple1), id(tuple2)) 
+
+# Immutable objects that are equal have different ids
+x = 2 ** 1000
+y = 2 ** 1000
+print("\n")
+#print(x, y)
+print(x == y)  # True, because they are equal
+print(id(x), id(y))  # Different ids, because they are immutable objects
+print(x is y)  # False, because they are different objects
+
+x = (1, 2, "python" * 1000)
+y = (1, 2, "python" * 1000)
+print("\n")
+#print(x, y)
+print(x == y)  # True, because they are equal
+print(id(x), id(y))  # Different ids, because they are immutable objects
+print(x is y)  # False, because they are different objects
