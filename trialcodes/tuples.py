0bd7eed0f9255b944126530fdcc603281eeda7df
FinnArth <=>
2025-06-08 13:58:40 +0500

    tuples unpacking

--- trialcodes/tuples.py
+++ trialcodes/tuples.py
@@ -6,3 +6,11 @@ print(type(empty_tuple))
 
 string_tuple = ("here")
 print(type(string_tuple))
+
+# Tuple unpacking
+some_tuple = (1, 2, 3)
+print(some_tuple)
+print(*some_tuple)
+
+(a,b,c) = some_tuple
+print(a,b,c)
