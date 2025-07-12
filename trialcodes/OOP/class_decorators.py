e8d90805e0e655646965293097d906873f4a91f3
FinnArth <=>
2025-07-12 19:05:45 +0500

    class_decorators are learned

--- trialcodes/OOP/class_decorators.py
+++ trialcodes/OOP/class_decorators.py
@@ -1,23 +1,49 @@
+from abc import ABC, abstractmethod
 class Person:
     origin_country = "USA"
 
-    def __init__(self, name, age, gender):
+    def __init__(self, name, surname, age, gender):
         self.name = name
+        self.surname = surname
         self.age = age
         self.gender = gender
 
     def speak(self, words):
         print(f"{self.name} speaks: {words}")
 
+    @property
+    def email(self):
+        return f'{self.name}.{self.surname}@email.com'
+
     @classmethod
     def change_origin_country(cls, new_country):
         cls.origin_country = new_country
         print(cls.origin_country)
 
-Berik = Person("Berik", 2323, "M")
-Oiken = Person("Oiken", 21, "M")
+    @staticmethod
+    def is_adult(age):
+        return age > 18
+
+Berik = Person("Berik", "Baltabay", 2323, "M")
+Oiken = Person("Oiken", "Amantay", 21, "M")
 print(Berik.origin_country)
 print(Oiken.origin_country)
 
 Berik.change_origin_country("UK")
 print(Oiken.origin_country)
+
+print(Person.is_adult(23))
+print(Berik.email)
+
+
+class Animal(ABC):
+    @abstractmethod
+    def eat(self):
+        raise NotImplementedError
+    
+class Dog(Animal):
+    def eat(self):
+        print("Dog is eating!")
+
+doggy = Dog()
+doggy.eat()
